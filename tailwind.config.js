1de3fec7174a4271be8ba2385a9bf6a297e4328a
Jackson <jacksontbooth@icloud.com>
2022-01-10 13:53:25 +1100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+    fontFamily: {
+      'styled': ['Prompt'],
+    },
+
+    extend: {
+      colors: {
+        'greydome': '#252525',
+        'bone': '#e1e1d3',
+      }
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
